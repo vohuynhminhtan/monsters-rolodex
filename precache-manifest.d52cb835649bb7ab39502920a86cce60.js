57177cd50dbd5b51735c22efd9924a896486d641
Võ Huỳnh Minh Tân <vohuynhminhtan@gmail.com>
2019-08-20 22:27:02 +0700

    Updates

---
+++ precache-manifest.d52cb835649bb7ab39502920a86cce60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed95b37fae6eec9905898bc88633840",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "526c19847c1c27c0380c",
+    "url": "/monster-rolodex/static/css/main.19ed9bb2.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monster-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "526c19847c1c27c0380c",
+    "url": "/monster-rolodex/static/js/main.a2d4e467.chunk.js"
+  },
+  {
+    "revision": "2a0841746d3b50a5d128",
+    "url": "/monster-rolodex/static/js/runtime~main.cc3a76ac.js"
+  }
+]);
